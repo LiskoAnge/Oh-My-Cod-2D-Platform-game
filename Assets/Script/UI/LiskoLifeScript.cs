3ad7cb5d3e503bcb498cad73ba20daaf134ef1f0
Angela Dematte <ange.dema@hotmail.com>
2022-07-04 16:20:09 +0100

    starting first level

--- Assets/Script/UI/LiskoLifeScript.cs
+++ Assets/Script/UI/LiskoLifeScript.cs
@@ -3,54 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 
 public class LiskoLifeScript : MonoBehaviour
 {
-
-
     public static int liskoValue = 3;
-
-    Text liskoLife;
-
+    TextMeshProUGUI liskoLife;
 
     void Start()
     {
-
-
-     
-   
-        liskoLife = GetComponent<Text>();
+        liskoLife = GetComponent<TextMeshProUGUI>();
         liskoLife.text = "" + liskoValue;
-
       //  liskoValue = PlayerPrefs.GetInt("liskoAmount");
-
-
     }
-
 
     void Update()
     {
-       
-
-
         if (liskoValue <= 0)
         {
-
-
-
             SceneManager.LoadScene(10);
-
             liskoValue = 3;
-
-
-
         } 
-
-
-
-
-
-
     }
 }
--- Assets/Script/UI/ScoreScript.cs
+++ Assets/Script/UI/ScoreScript.cs
@@ -3,63 +3,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ScoreScript : MonoBehaviour
 {
     public static int scoreValue = 0;
-    Text score;
 
+    TextMeshProUGUI score;
  
-
-
-
-
     void Start()
     {
-
-
-        score = GetComponent<Text>();
-
+        score = GetComponent<TextMeshProUGUI>();
         scoreValue = PlayerPrefs.GetInt("coinsAmount");
-
-        
     }
 
-    
     void Update()
     {
         score.text = "" + scoreValue;
         PlayerPrefs.SetInt("coinsAmount", scoreValue);
-
-
-
         if (PlayerHB.lifebar > 0)
         {
            // totalscore = (int)Time.time + enemiesKilledScore;
            // scoretext.text = totalScore.ToString();
-
-
-
             score.text = "" + scoreValue;
             PlayerPrefs.SetInt("coinsAmount", scoreValue);
-
-
         }
-
-
-
         if (scoreValue <= 0)
         {
-
-
             scoreValue = 0;
         }
-
-
-
-
-
-
-
     }
 }
